35edd3efbb3e7e779545f131ed07b16b7ca8aca3
WenjingSu <18811538591@163.com>
2019-12-24 10:35:50 +0800

    header iconfont

---
+++ src/pages/home/components/header.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <span class="iconfont">&#xe629;</span>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe60d;</span>输入城市/景点/游玩主题</div>
+    <div class="header-right">
+      <span class="iconfont arrowicon">&#xe624;</span>城市</div>
+  </div>
+</template>
+
+<script>
+export default{
+  name:'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/variables.styl'
+  .header
+      display:flex
+      line-height:.86rem
+      background:$bgColor
+      color:#fff
+      .header-left
+        width:.64rem
+        float:left
+      .header-input
+        flex:1
+        height:.64rem
+        line-height:.64rem
+        background:#fff
+        margin-top:.12rem
+        margin-left:.1rem
+        border-radius:.1rem
+        color:#ccc
+      .header-right
+        width:1.24rem
+        float:right
+        text-align:center
+        .arrowicon
+          font-size:.24rem
+          margin-right:.12rem
+
+
+
+</style>
